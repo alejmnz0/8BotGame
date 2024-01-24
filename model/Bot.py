00a45440592d2b7000df140baf9acfca8e099e2a
A.Jimenez <jimenezalejandro505@gmail.com>
2024-01-24 14:28:36 +0100

    Añadidas imagenes y añadido movimiento al robot

--- model/Bot.py
+++ model/Bot.py
@@ -1,13 +1,19 @@
-import pygame
-
-
 class Bot:
-    hp = 10
-    waterproof = False
-
-    def __init__(self):
+    def __init__(self, sprite):
         self.position = [0, 0]
-        self.speed = 10
+        self.speed = 2
+        self.hp = 10
+        self.sprite = sprite
+        self.size = [100, 100]
 
     def move_right(self):
         self.position[0] += self.speed
+
+    def move_left(self):
+        self.position[0] -= self.speed
+
+    def move_up(self):
+        self.position[1] -= self.speed
+
+    def move_down(self):
+        self.position[1] += self.speed
--- model/initGame.py
+++ model/initGame.py
@@ -2,22 +2,44 @@ import pygame
 
 from model.Bot import Bot
 
-robot = Bot()
 pygame.init()
+sprite = pygame.image.load("../assets/default.png")
+robot = Bot(sprite)
 
-
-ventana = pygame.display.set_mode((1280, 720))
+ventana = pygame.display.set_mode((1600, 900))
+superficie = pygame.Surface((1600, 900))
 pygame.display.set_caption("8BotGame")
+font = pygame.font.Font(None, 50)
 
 jugando = True
 while jugando:
-
+    ventana.fill((255, 255, 255))
+    ventana.blit(robot.sprite, robot.position)
     for event in pygame.event.get():
+    #     if event.type == pygame.KEYDOWN:
+    #         match event.key:
+    #             case pygame.K_RIGHT:
+    #                 robot.move_right()
+    #             case pygame.K_LEFT:
+    #                 robot.move_left()
+    #             case pygame.K_UP:
+    #                 robot.move_up()
+    #             case pygame.K_DOWN:
+    #                 robot.move_down()
 
         if event.type == pygame.QUIT:
             jugando = False
 
-    ventana.fill((255, 255, 255))
+    keys = pygame.key.get_pressed()
+    if keys[pygame.K_d]:
+        robot.move_right()
+    if keys[pygame.K_a]:
+        robot.move_left()
+    if keys[pygame.K_w]:
+        robot.move_up()
+    if keys[pygame.K_s]:
+        robot.move_down()
+
 
     pygame.display.flip()
 
